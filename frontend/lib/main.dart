d3b6a31de64577b7dd650ee667a9f6c6bd74d02f
N4647 <nitinyadav4637@gmail.com>
2025-06-08 01:09:22 +0530

    feat: added routes in main.dart and updated as per course bloc

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:frontend/data/dataProvider/auth_provider.dart';
-import 'package:frontend/data/repositories/auth_repository.dart';
-import 'package:frontend/presentation/pages/signup_screen.dart';
-import 'presentation/pages/login_screen.dart';
-import 'presentation/pages/home_screen.dart'; 
+import './data/dataProvider/auth_provider.dart';
+import './data/dataProvider/course_provider.dart';
+import './data/repositories/auth_repository.dart';
+import './data/repositories/course_repository.dart';
+import './presentation/pages/signup_screen.dart';
+import './presentation/pages/login_screen.dart';
+import './presentation/pages/home_screen.dart';
+import './presentation/pages/enrolled_screen.dart';
+import './presentation/pages/drop_screen.dart';
 import './presentation/bloc/auth/auth_bloc.dart';
 import './presentation/bloc/auth/auth_events.dart';
 import './presentation/bloc/auth/auth_states.dart';
+import './presentation/bloc/course/course_bloc.dart'; 
+import './presentation/pages/add_screen.dart';
 
 import 'package:http/http.dart' as http;
 
@@ -17,20 +23,34 @@ void main() {
 
 class MyApp extends StatelessWidget {
   MyApp({super.key});
-
+  final courseProvider = CourseProvider(client: http.Client());
   final authProvider = AuthProvider(client: http.Client());
   late final authRepository = AuthRepository(authProvider);
+  late final courseRepository = CourseRepository(courseProvider);
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) {
-        final bloc = AuthBloc(authRepository);
-        bloc.add(AppStarted());
-        return bloc;
-      },
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<AuthBloc>(
+          create: (context) {
+            final bloc = AuthBloc(authRepository);
+            bloc.add(AppStarted());
+            return bloc;
+          },
+        ),
+        BlocProvider<CourseBloc>(
+          create: (context) => CourseBloc(courseRepository), 
+        ),
+      ],
       child: MaterialApp(
         title: 'Student Enroll App',
+        routes: {
+          '/home': (context) => const HomeScreen(),
+          '/enrolledCourses': (context) => const EnrolledCoursesScreen(),
+          '/availableCourses': (context) => const EnrollCourseScreen(),
+           '/dropCourse': (context) => const DropCourseScreen(),
+        },
         home: BlocBuilder<AuthBloc, AuthState>(
           builder: (context, state) {
             if (state is EntryLoad) {
@@ -38,11 +58,11 @@ class MyApp extends StatelessWidget {
                 body: Center(child: CircularProgressIndicator()),
               );
             } else if (state is LoggedIn) {
-              return HomeScreen();
+              return const HomeScreen();
             } else if (state is SignUpState) {
-              return SignUpScreen();
+              return const SignUpScreen();
             } else {
-              return LoginScreen();
+              return const LoginScreen();
             }
           },
         ),
